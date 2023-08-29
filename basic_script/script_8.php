2254899619da5e41d52d3cec10ae5ac162302265
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-08-29 12:31:11 +0530

    basic script in php
    script 1 to 14

---
+++ basic_script/script_8.php
@@ -0,0 +1,10 @@
+<!-- Develop a PHP program that assigns a name to the variable $name and uses it to display a dynamic
+greeting message like "Hello, John!". -->
+
+<?php
+$name = "rahul";
+$greeting = "Hello, " . $name . "!";
+
+echo $greeting;
+?>
+
