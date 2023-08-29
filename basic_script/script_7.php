0125bc07dcca4aafbabb11be665617fe773976e1
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-08-29 12:28:55 +0530

    Delete basic_script directory

--- basic_script/script_7.php
+++
@@ -1,9 +0,0 @@
-<!-- Create a PHP program that assigns three words to variables ($word1, $word2, and $word3) and
-uses concatenation to display them as a sentence: "This is a sentence." -->
-<?php
-$word1 = "This";
-$word2 = "is";
-$word3 = "a sentence";
-
- echo $word1 . " " . $word2 . " " . $word3 . "."; 
-?>
