5ec32f3cdec26fe724268a04d254b52a90df9139
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-09-20 12:50:24 +0530

    Add files via upload

---
+++ table_script.php
@@ -0,0 +1,26 @@
+<?php 
+echo "<table border=1>";
+ $letter = 'A';
+ for($i=1; $i<=7; $i++)
+ {
+    echo"<tr>";
+    for ($j=1;$j<=4; $j++)
+    {
+
+        if($letter < 'z')
+        {
+            echo '<td>'.$letter.'</td>';
+            $letter++;
+            
+        }
+        else
+        {
+           
+            echo '<td></td>';
+            
+        }
+    }
+    echo '<tr>';
+ }
+ echo "</table>";
+?>
