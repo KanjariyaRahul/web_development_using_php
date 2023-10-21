c86aa42539f118275c265a258b7598693be9ddd0
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-10-21 19:01:41 +0530

    Update table_script_2.php

--- college_work/table_script_2.php
+++ college_work/table_script_2.php
@@ -1,1 +1,31 @@
 
+<?php 
+echo "<table border=1>";
+ $letter = 'A';
+ for($i=1; $i<=7; $i++)
+ {
+    echo"<tr>";
+    for ($j=1;$j<=4; $j++)
+    {
+
+        if($letter < 'z')
+        {
+            echo '<td>'.$letter.'</td>';
+            $letter++;
+        if($letter=='AA')
+        {
+            break;
+        }
+            
+        }
+        else
+        {
+           
+            echo '<td></td>';
+            
+        }
+    }
+    echo '<tr>';
+ }
+ echo "</table>";
+?>
