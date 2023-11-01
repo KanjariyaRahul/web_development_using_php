7d810627bafb1ea5fb96fc82ebc78d3cfa70e40b
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-11-01 11:42:29 +0530

    Add files via upload

--- 15618223042/database/15618223042.sql
+++ 15618223042/database/15618223042.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2023 at 07:15 AM
+-- Generation Time: Nov 01, 2023 at 07:10 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `15618223042`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_details`
+--
+
+CREATE TABLE `product_details` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_description` text NOT NULL,
+  `product_category` varchar(100) NOT NULL,
+  `product_price` decimal(10,0) NOT NULL,
+  `product_sku` varchar(50) NOT NULL,
+  `product_barcode` varchar(50) NOT NULL,
+  `product_weight` decimal(10,0) NOT NULL,
+  `product_dimensions` varchar(100) NOT NULL,
+  `product_quantity` int(11) NOT NULL,
+  `product_variants` varchar(255) NOT NULL,
+  `shipping_info` varchar(255) NOT NULL,
+  `seo_info` varchar(255) NOT NULL,
+  `product_brand` varchar(100) NOT NULL,
+  `product_features` text NOT NULL,
+  `product_benefites` text NOT NULL,
+  `related_products` varchar(255) NOT NULL,
+  `product_reviews` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `image_filenames` varchar(2555) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -43,8 +72,21 @@ CREATE TABLE `registration` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`reg_id`, `first_name`, `middle_name`, `last_name`, `email`, `mobil_no`, `password`, `city`, `state`, `country`, `education`, `gender`) VALUES
+(1, 'rahul', 'mansukhbhai', 'kanjariya', 'rahul@gmail.com', '9265614292', '123', 'jamnagar', 'gujarat', 'india', 'bca', 'male');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `product_details`
+--
+ALTER TABLE `product_details`
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indexes for table `registration`
@@ -57,10 +99,16 @@ ALTER TABLE `registration`
 --
 
 --
+-- AUTO_INCREMENT for table `product_details`
+--
+ALTER TABLE `product_details`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `reg_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `reg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
